bd5cf53d3efebc329b64a18bece2d7a70c173a84
Alexandre H. T. Dias <alexandrehtdias@gmail.com>
2022-01-18 10:43:13 -0300

    feat: download TACO directly from the website

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import camelot
+import requests
+
+
+class TACO:
+    URL = 'https://www.nepa.unicamp.br/taco/contar/taco_4_edicao_ampliada_e_revisada.pdf?arquivo=1'
+    FILENAME = 'taco_4_ed.pdf'
+
+    @staticmethod
+    def _download_pdf():
+        response = requests.get(TACO.URL)
+        return response.content
+
+    @staticmethod
+    def _write_pdf(content):
+        with open(TACO.FILENAME, 'wb') as file:
+            file.write(content)
+
+    @staticmethod
+    def get_document():
+        TACO._write_pdf(TACO._download_pdf())
 
 
 if __name__ == '__main__':
-    filename = 'taco_4_edicao_ampliada_e_revisada.pdf'
-    tables = camelot.read_pdf(filepath=filename, pages='all', flavor='stream')
+    TACO.get_document()
+
+    tables = camelot.read_pdf(filepath=TACO.FILENAME, pages='all', flavor='stream')
 
     print()
